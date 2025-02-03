be83185a635b3b221c8ab1785389de52d1324db2
Th. Androutsos <a8anassis@gmail.com>
2025-02-03 20:02:43 +0200

    feat: in Singly linked list several methods added (insertFirst, insertLast, removeFirst, removeLast)

--- src/gr/aueb/cf/ch19/singly_linked_list/SingleList.java
+++ src/gr/aueb/cf/ch19/singly_linked_list/SingleList.java
@@ -15,5 +15,57 @@ public class SingleList<T> {
         head = tmp;
     }
 
+    /**
+     * Time complexity O(n)
+     * @param t the value to be inserted.
+     */
+    public void insertLast(T t) {
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(null);
+
+        if (head == null) {
+            insertFirst(t);
+            return;
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext() != null; n = n.getNext()) {
+
+        }
+        n.setNext(tmp);
+    }
+
+
+    /**
+     * Time complexity O(1)
+     * @return
+     */
+    public Node<T> removeFirst() {
+        if (head == null) return null;
+
+        Node<T> node = head;
+        head = head.getNext();
+        return node;
+    }
+
+    /**
+     * Time-complexity O(n)
+     * @return the removed node.
+     */
+    public Node<T> removeLast() {
+        if (head == null || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext().getNext() != null; n = n.getNext()) {
+        }
+
+        Node<T> nodeToReturn = n.getNext();
+        n.setNext(null);
+        return nodeToReturn;
+    }
+
 
 }
