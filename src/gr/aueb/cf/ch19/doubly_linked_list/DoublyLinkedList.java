99f470aaf1b9a12ef924c9d287919b9dcda2e587
Th. Androutsos <a8anassis@gmail.com>
2025-02-04 18:44:25 +0200

    feat: removefirst, remove last added.

--- src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
+++ src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
@@ -39,4 +39,51 @@ public class DoublyLinkedList<T> {
         tail.setNext(tmp);
         tail = tmp;
     }
+
+    /**
+     * Time-complexity O(1)
+     * @return  the removed node.
+     */
+    public Node<T> removeFirst() {
+        Node<T> nodeToReturn;
+
+        if (isEmpty()) return null;
+
+        nodeToReturn = head;
+        if (head.getNext() == null) tail = null;
+        head = head.getNext();
+        return nodeToReturn;
+    }
+
+    /**
+     * Time-complexity O(1)
+     * @return  the removed node.
+     */
+    public Node<T> removeLast() {
+        Node<T> nodeToReturn;
+        if (isEmpty() || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        nodeToReturn = tail;
+        tail = tail.getPrev();
+        tail.setNext(null);
+        return nodeToReturn;
+    }
+
+    public Node<T> get(T t) {
+        Node<T> nodeToReturn = null;
+
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            if (n.getNext().equals(t)) {
+                nodeToReturn = n;
+                break;
+            }
+        }
+        return nodeToReturn;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
