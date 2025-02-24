f770894e67c21440958f8929a174c23f2e7c22a1
Th. Androutsos <a8anassis@gmail.com>
2025-02-24 17:58:42 +0200

    feat: Abstract classes added.

---
+++ src/gr/aueb/cf/solutions/ch15/Cat.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.solutions.ch15;
+
+public class Cat extends Animal {
+
+    public Cat() {
+        //super();
+    }
+
+    public Cat(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Cat is speaking.");
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + getId() + ", name: " + getName();
+    }
+}
---
+++ src/gr/aueb/cf/solutions/ch15/Dog.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.solutions.ch15;
+
+public class Dog extends Animal {
+
+    public Dog() {
+        //super();
+    }
+
+    public Dog(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Dog is speaking.");
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + getId() + ", name: " + getName();
+    }
+}
