105a2ca1e5083c918c7771795318142db2e92af1
Th. Androutsos <a8anassis@gmail.com>
2025-04-03 09:43:18 +0300

    fix: new versions

--- src/gr/aueb/cf/ch2/ExpressionsApp2.java
+++ src/gr/aueb/cf/ch2/ExpressionsApp2.java
@@ -6,8 +6,9 @@ package gr.aueb.cf.ch2;
 public class ExpressionsApp2 {
 
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
+        int num1 = 25;
+        int num2 = 10;
+        int baseNum = 12;
         int sum = 0;
         int sub = 0;
         int mul = 0;
@@ -16,25 +17,32 @@ public class ExpressionsApp2 {
         int result1 = 0;
         int result2 = 0;
 
-        sum = num1 + num2;
-        sub = num1 - num2;
-        mul = num1 * num2;
-        div = num1 / num2;
+        sum = num1 + num2;  // 35
+        sub = num1 - num2;  // 15
+        mul = num1 * num2;  // 250
+        div = num1 / num2;  // 2
+        mod = num1 % num2;  // 5
 
-        num1++;
-        num1 = num1 + 1;
-        num1 += 1;
+        sum = sum + 1;    // 36
+        mul += 1;           // 251
 
-        --num2;
+        System.out.printf("Sum: %d, Sub: %d, Mul: %d, Div: %d, Mod: %d\n", sum, sub, mul, div, mod);
 
-        num1++;
-        result1 = num1;
+        baseNum++;      // 13
+                 // 27
+        --num2;         // 9
 
-        result1 = num1++;   // έστω num1 = 8, result1 = 8, num1 = 9
-        result2 = ++num2;   // έστω num2 = 8, result2 = 9, num2 = 9
+        System.out.printf("baseNum: %d, num1: %d, num2: %d\n", baseNum, num1, num2);
+
+        result1 = num1++;   // result1 = 27, num1 = 28
+        result2 = ++num2;   // result2 = 10, num2 = 10
+
+        System.out.printf("result1: %d, result2: %d\n", result1, result2);
 
         result1 = result1 * 12;
-        result1 *= 12;
+       // result1 *= 12;
+
+        System.out.printf("result1: %d", result1);
 
     }
 }
