e93d1a640dc45dc8d3ec6bc1f70591322fdfd049
Th. Androutsos <a8anassis@gmail.com>
2025-04-07 17:01:51 +0300

    fix: new versions

--- src/gr/aueb/cf/ch2/MathDemo9.java
+++ src/gr/aueb/cf/ch2/MathDemo9.java
@@ -5,7 +5,7 @@ package gr.aueb.cf.ch2;
  */
 public class MathDemo9 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Το απόλυτο του -5 είναι: " + Math.abs(-5));
         System.out.println("Το min του 3, 19 είναι: " + Math.min(3, 19));
         System.out.println("Το max του -5, 299 είναι: " + Math.max(-5, 299));
