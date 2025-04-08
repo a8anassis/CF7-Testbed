d2f172ecff281ea5f82124ffef510a8678fbd810
Th. Androutsos <a8anassis@gmail.com>
2025-04-08 12:03:36 +0300

    feat: new feat

---
+++ src/gr/aueb/cf/ch3/BoolApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch3;
+
+/**
+ * Boolean Demo.
+ */
+public class BoolApp {
+
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean isRaining = false;
+    }
+}
