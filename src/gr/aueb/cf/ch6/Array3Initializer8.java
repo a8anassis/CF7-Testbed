8fa552c839b265166f0dcde3cac03af93fc14c82
Th. Androutsos <a8anassis@gmail.com>
2025-04-22 20:30:28 +0300

    feat: array initializer created

---
+++ src/gr/aueb/cf/ch6/Array3Initializer8.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch6;
+
+/**
+ * Demo array initializer.
+ */
+public class Array3Initializer8 {
+
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[] {1, 2, 3, 4, 5, 6};
+
+        System.out.println(arr[0] + " " + arr[1] + "..." + arr[arr.length -1]);
+    }
+}
