00bb4994a3d477872cca2a3536f0784b61a32d99
Th. Androutsos <a8anassis@gmail.com>
2025-04-22 21:49:46 +0300

    feat: Two-dim jagged arrays

--- src/gr/aueb/cf/ch6/Array13JaggedArray8.java
+++ src/gr/aueb/cf/ch6/Array13JaggedArray8.java
@@ -1,6 +1,10 @@
 package gr.aueb.cf.ch6;
 
-public class JaggedArray {
+/**
+ * Jagged Arrays are the arrays that may include
+ * different dimensions. Not grids in the traditional manner.
+ */
+public class Array13JaggedArray8 {
 
     public static void main(String[] args) {
         int[][] arr = new int[3][];
@@ -8,6 +12,13 @@ public class JaggedArray {
         arr[0] = new int[5];
         arr[1] = new int[3];
         arr[2] = new int[6];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         for (int[] row : arr) {
             for (int el : row) {
