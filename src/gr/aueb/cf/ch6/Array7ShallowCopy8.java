4b5dd4906362625f330bbcbd4e2cfb5d15653cd1
Th. Androutsos <a8anassis@gmail.com>
2025-04-22 20:57:53 +0300

    feat: Array shallow copy

---
+++ src/gr/aueb/cf/ch6/Array7ShallowCopy8.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch6;
+
+/**
+ * Copies an array. Actually what is copied
+ * is not the values but the reference.
+ * When we copy references, it is always a shallow copy.
+ * And there are side effects.
+ */
+public class Array7ShallowCopy8 {
+
+    public static void main(String[] args) {
+
+        int[] initialArray = {1, 2, 3, 4, 5};
+        int[] arrayCopy = initialArray;         // Shallow Copy
+
+        arrayCopy[0] = 100;
+
+        for (int val : initialArray) {
+            System.out.print(val + " ");
+        }
+    }
+}
