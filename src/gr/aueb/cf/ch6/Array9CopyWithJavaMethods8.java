f29a9bd8057b45843485e74ebae40575371a4189
Th. Androutsos <a8anassis@gmail.com>
2025-04-22 21:08:26 +0300

    feat: Array copy with methods

---
+++ src/gr/aueb/cf/ch6/Array9CopyWithJavaMethods8.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.ch6;
+
+public class Array9CopyWithJavaMethods8 {
+
+    public static void main(String[] args) {
+        int[] initialArray = {1, 2, 3, 4, 5};
+        int[] arrayCopy;
+
+        arrayCopy = arrDeepCopy(initialArray);
+
+        for (int el : arrayCopy) {
+            System.out.print(el + " ");
+        }
+    }
+
+    public static int[] arrDeepCopy(int[] sourceArray) {
+
+        int[] destinationArray = new int[sourceArray.length];
+
+        System.arraycopy(sourceArray, 0, destinationArray, 0, sourceArray.length);
+//        destinationArray = Arrays.copyOf(sourceArray, sourceArray.length);
+//        destinationArray = Arrays.copyOfRange(sourceArray, 0, sourceArray.length);
+
+        return destinationArray;
+    }
+}
