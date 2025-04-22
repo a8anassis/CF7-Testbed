6d05e426590cdc287ccab264604cf534680aead7
Th. Androutsos <a8anassis@gmail.com>
2025-04-22 19:39:38 +0300

    feat: method overloading created

--- src/gr/aueb/cf/ch5/MethodOverloading8.java
+++ src/gr/aueb/cf/ch5/MethodOverloading8.java
@@ -3,10 +3,19 @@ package gr.aueb.cf.ch5;
 /**
  * Demo of method overloading.
  */
-public class MethodOverloading {
+public class MethodOverloading8 {
 
     public static void main(String[] args) {
+        int sum1, sum2;
+        long sum3;
+        int sum4;
 
+        sum1 = add(1, 3);
+        sum2 = add(1, 4L);
+        sum3 = add(1L, 5L);
+        sum4 = add(1, 3, 5);
+
+        System.out.printf("%d \t%d \t%d \t%d\n", sum1, sum2, sum3, sum4);
     }
 
     public static int add(int a, int b) {
