674d09674fecd56a0c0d678102e590318caa6a87
Th. Androutsos <a8anassis@gmail.com>
2025-04-30 13:42:41 +0300

    fix: update

--- src/gr/aueb/cf/ch6/Array19CircularRotation8.java
+++ src/gr/aueb/cf/ch6/Array19CircularRotation8.java
@@ -4,33 +4,30 @@ public class Array19CircularRotation8 {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
-        int[] leftShifted = doLeftShift(arr, 2);
-        int[] rightShifted = doRightShift(arr, 3);
+        int[] leftShifted = shiftLeftBy(arr, 2);
+        int[] rightShifted = shiftRightBy(arr, 3);
         printArr(leftShifted);
         printArr(rightShifted);
     }
 
-    public static int[] doLeftShift(int[] arr, int offset) {
-        if (arr == null) return null;
-        if (offset < 0) return null;
-
-
-        int[] rotated = new int[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            rotated[i] = arr[(i + offset) % arr.length];
-        }
-        return rotated;
-    }
-
-    public static int[] doRightShift(int[] arr, int offset) {
-        if (arr == null) return null;
-        if (offset < 0) return null;
-
+    public static int[] shiftRightBy(int[] arr, int offset) {
+        if (arr == null) return new int[0];
+        if (offset < 0) return new int[0];
         int[] rotated = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
             rotated[(i + offset) % arr.length] = arr[i];
+        }
+        return rotated;
+    }
+
+    public static int[] shiftLeftBy(int[] arr, int offset) {
+        if (arr == null) return new int[0];
+        if (offset < 0) return new int[0];
+        int[] rotated = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            rotated[i] = arr[(i + offset) % arr.length];
         }
         return rotated;
     }
