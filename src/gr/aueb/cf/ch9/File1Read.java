d517990a09eea4d2b20d21589d6493dfe7845a37
Th. Androutsos <a8anassis@gmail.com>
2025-05-06 16:17:25 +0300

    updates

---
+++ src/gr/aueb/cf/ch9/File1Read.java
@@ -0,0 +1,67 @@
+package gr.aueb.cf.ch9;
+
+import javax.swing.text.Style;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class File1Read {
+
+    public static void main(String[] args) throws IOException {
+        String file = "C:/Users/a8ana/OneDrive/Υπολογιστής/tmp/test.txt";
+
+        fileReaderRead(file);
+        System.out.println();
+        scannerRead(file);
+        bufferedReaderRead(file);
+    }
+
+    // Char by char - Slow
+    public static void fileReaderRead(String file) throws IOException {
+
+        // try with resources
+        try (FileReader fr = new FileReader(file)) {
+            int c;
+            while ((c = fr.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            System.err.println(LocalDateTime.now() + "fileReaderRead" + e);
+        }
+//        finally {
+//            try {
+//                fr.close();
+//            } catch (Exception e) {
+//                System.out.println(e);
+//            }
+//        }
+    }
+
+    // For structured text with primitives - > int double string structured
+    public static void scannerRead(String file) throws FileNotFoundException {
+        try (Scanner sc = new Scanner(new File(file))) {
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(LocalDateTime.now() + "," + e);
+            throw e;
+        }
+    }
+
+    // for large texts, powerful read-line
+    public static void bufferedReaderRead(String file) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            // Powerful read-line
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(LocalDateTime.now() + "," + e);
+            throw e;
+        }
+    }
+}
